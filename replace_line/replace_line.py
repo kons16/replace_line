ec20de6b67fc15448e60c7307cc56a462e97b4c6
kons16 <river.field.1126@gmail.com>
2020-03-16 14:57:24 +0900

    fix replace_line.py

--- replace_line/replace_line.py
+++ replace_line/replace_line.py
@@ -1,20 +1,7 @@
-from argparse import ArgumentParser
+""" replace_line function """
 
 
-def get_option():
-    argparser = ArgumentParser()
-    argparser.add_argument('-i', '--input', type=str,
-                           help='Input file name')
-    argparser.add_argument('-o', '--output', type=str,
-                           help='Output file name')
-    argparser.add_argument('-l', '--line', type=str,
-                           help='The line you want to replace')
-    argparser.add_argument('-r', '--replace', type=str,
-                           help='replace string')
-    return argparser.parse_args()
-
-
-def replace_func(input_file_name, output_file_name, replace_line, replace_str):
+def replace_line(input_file_name, output_file_name, replace_line, replace_str):
     """ 行を指定された文字に書き換える """
     f = open(input_file_name, "r")
     fw = open(output_file_name, "w")
@@ -26,6 +13,8 @@ def replace_func(input_file_name, output_file_name, replace_line, replace_str):
         if replace_line == "blank":
             if line != "":
                 fw.write(line + "\n")
+            elif line == "" and replace_str is not None:
+                fw.write(replace_str + "\n")
         else:
             if line == replace_line:
                 fw.write(replace_str + "\n")
@@ -34,14 +23,3 @@ def replace_func(input_file_name, output_file_name, replace_line, replace_str):
 
     f.close()
     fw.close()
-
-
-if __name__ == '__main__':
-    args = get_option()
-
-    input_file_name = args.input
-    output_file_name = args.output
-    replace_line = args.line
-    replace = args.replace
-
-    replace_func(input_file_name, output_file_name, replace_line, replace)
